90fe9b2092202942a4891f925179140c5595a47c
deenoo82 <deenoo82@users.noreply.github.com>
2016-04-24 13:49:07 -0700

    initial commit

---
+++ server.r
@@ -0,0 +1,43 @@
+library(shiny)
+library(quanteda)
+
+# loading the prediction matric
+load('predictMatrix.rda')
+
+predictNextword <- function(input) {
+  tokens <- tokenize(input, what="word", removeNumber=TRUE,
+                  removePunct=TRUE, removeSeparators=TRUE, 
+                  removeTwitter=FALSE, ngrams=3L)
+  tokens <- tolower(tokens)
+  size <- length(unlist(tokens))
+  lasttoken <- tokens[[1]][size]
+  last2word <- paste(unlist(strsplit(lasttoken,'_'))[2:3],collapse = "_")
+  lastword <- unlist(strsplit(lasttoken,'_'))[2]
+  tbl <- data.frame(tokens)
+  colnames(tbl) <- c("token")
+  # merging two results
+  tbl <- merge(tbl, predictMatrix, by.x = "token", by.y = "word")
+  sumprod <- prod(tbl$prob)
+  # get the potential results from prediction matrix
+  prediction <- predictMatrix[which(predictMatrix$key == last2word),]
+  # use 2-gram when no result if found
+  if (nrow(prediction) == 0) {
+    prediction <- predictMatrix[which(predictMatrix$key == lastword),]
+  }
+  # check for the new prediction table
+  if (nrow(prediction) == 0) {
+    ng <- ngram(input, n=1)
+    return (babble(ng,1))
+  }
+  prediction$nextwordProb <- prediction$prob * sumprod
+  prediction <- prediction[order(-prediction[,5]),]
+  return (prediction[1,3])
+}
+
+shinyServer(
+  function(input, output) {
+     output$inputValue <- renderPrint(input$userInput)
+     # tokenize the user input
+     output$nextword <- renderPrint(predictNextword(input$userInput))
+  }
+)
---
+++ ui.r
@@ -0,0 +1,22 @@
+library(shiny)
+
+# Define UI for application that takes in input
+shinyUI(
+  pageWithSidebar(
+    # application title
+    headerPanel("Text Prediction based on ngram model"),
+    
+    sidebarPanel(
+      textInput('userInput','Please provide text input for next word prediction'),
+      submitButton('Submit')
+  ),
+  mainPanel(
+    p('This app uses n-gram model to try and predict the next word based on user input'),
+    p('Please input text into the text input and hit submit button'),
+    h3('Results of Suggestion'),
+    h4('You have entered'),
+    verbatimTextOutput("inputValue"),
+    h4('The next word is '),
+    textOutput("nextword")
+    )
+))
